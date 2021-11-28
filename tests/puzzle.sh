c5bfe9c23dc0605435dfadae141b30aa92781567
Daniel Dugovic <dandydand@gmail.com>
2021-11-28 09:47:05 -0600

    Attempt to fix expect: spawn id exp3 not open

--- tests/puzzle.sh
+++ tests/puzzle.sh
@@ -10,15 +10,13 @@ trap 'error ${LINENO}' ERR
 
 echo "puzzle testing started"
 
-cat << EOF > puzzle.exp
+expect << EOF
    set timeout 600
    spawn python3 ../tests/chess-artist/chess_artist.py --infile ../tests/chess-artist/EPD/wacnew.epd --outfile wacnew_result.txt --enginefile ./stockfish --eval static --job test
    expect -re "Correct percentage    : (100|9\\\\d)"
    expect eof
 EOF
 
-expect puzzle.exp > /dev/null
-
-rm puzzle.exp wacnew_result.txt
+rm wacnew_result.txt
 
 echo "puzzle testing OK"
